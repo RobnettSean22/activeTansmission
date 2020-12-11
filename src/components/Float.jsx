f3d0aa9f71e33a713ef541f581e76c37e8494c47
RobnettSean22 <robnettsean22@gmail.com>
2020-12-12 03:49:56 +0900

    float template ready for reviews when I can get

--- src/components/Float.jsx
+++ src/components/Float.jsx
@@ -23,30 +23,72 @@ const TitleContainer = styled.div`
   height: 69px;
   background: rgb(36, 39, 140);
   border-radius: 31px 31px 0 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: rgb(250, 250, 250);
+  h1 {
+    font-family: "Montserrat", sans-serif;
+  }
 `;
 const Rating = styled.div`
   height: 53px;
   background: rgb(250, 250, 250);
+  display: flex;
+  justify-content: flex-start;
+  align-items: flex-end;
+  h2 {
+    font-size: 20px;
+    margin: 0 0 0 13px;
+  }
 `;
 const ReviewContainer = styled.div`
   height: 145px;
   border-top: solid 1px rgb(64, 63, 63);
   border-bottom: solid 1px rgb(64, 63, 63);
   background: rgb(250, 250, 250);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin: 17px 88.5px 17px 88.5px;
+    font-size: 20px;
+    line-height: 1.45;
+  }
 `;
 const NameContainer = styled.div`
   height: 83px;
   border-radius: 0 0 31px 31px;
   background: rgb(250, 250, 250);
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+  h2 {
+    font-family: "Montserrat", sans-serif;
+    font-style: italic;
+    font-size: 24px;
+    margin: 0 29px 0 0;
+  }
 `;
 
 const Float = (props) => {
   return (
     <Wrapper>
-      <TitleContainer></TitleContainer>
-      <Rating></Rating>
-      <ReviewContainer></ReviewContainer>
-      <NameContainer></NameContainer>
+      <TitleContainer>
+        <h1>Our Customers</h1>
+      </TitleContainer>
+      <Rating>
+        <h2>(Stars)</h2>
+      </Rating>
+      <ReviewContainer>
+        <p>
+          “Professional and quality service. If you want an honest mechanic
+          shop, go here. They’ll tell you what needs to be tended to.”
+        </p>
+      </ReviewContainer>
+      <NameContainer>
+        <h2>Pieter Veen</h2>
+      </NameContainer>
     </Wrapper>
   );
 };
