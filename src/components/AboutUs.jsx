baa01b5526fa60270df46711da646c62a123bf00
RobnettSean22 <robnettsean22@gmail.com>
2020-12-12 17:50:18 +0900

    html layout for the about us started. will turn eployee div in to a card for easier future up keep and make css a grid later

--- src/components/AboutUs.jsx
+++ src/components/AboutUs.jsx
@@ -10,17 +10,53 @@ const MissionContainer = styled.div`
   height: 647px;
   background: pink;
 `;
+const MissionTitle = styled.div`
+  width: 100%;
+  height: 161px;
+`;
+const MissionDescription = styled.div`
+  width: 100%;
+  height: 469px;
+`;
+const SectionBorder = styled.div`
+  width: 100%;
+  height: 47px;
+`;
 const EmployeeInfo = styled.div`
   width: 100%;
   height: 1391px;
   background: orange;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
 `;
+const EmployeeTitle = styled.div`
+  width: 1030.2px;
+  height: 161px;
+  margin-bottom: 52px;
+`;
+const Employee = styled.div`
+  width: 1030.2px;
+  height: 235px;
+  margin-bottom: 173px;
+`;
+
 const AboutUs = (props) => {
   return (
     <Wrapper>
       <Header />
-      <MissionContainer></MissionContainer>
-      <EmployeeInfo></EmployeeInfo>
+      <MissionContainer>
+        <MissionTitle></MissionTitle>
+        <MissionDescription></MissionDescription>
+        <SectionBorder></SectionBorder>
+      </MissionContainer>
+      <EmployeeInfo>
+        <EmployeeTitle></EmployeeTitle>
+        <Employee></Employee>
+        <Employee></Employee>
+        <Employee></Employee>
+      </EmployeeInfo>
       <Footer />
     </Wrapper>
   );
