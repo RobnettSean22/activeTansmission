7363f4c4ce3811a79ebc1ed78d250d16a0658d25
RobnettSean22 <robnettsean22@gmail.com>
2021-01-06 00:57:21 +0900

    creating data base form info and created a componet for easy render

---
+++ src/components/Information.jsx
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const Description = styled.div`
+  width: 100%;
+  height: 752px;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+`;
+const DescriptTitle = styled.div`
+  width: 218px;
+  height: 59px;
+  margin-left: 160px;
+  margin-bottom: 32px;
+  h2 {
+    margin: 0;
+    font-size: 51px;
+  }
+`;
+
+const Descript = styled.div`
+  width: 1038px;
+  height: 152px;
+  margin-left: 169px;
+  margin-bottom: 32px;
+  p {
+    font-size: 24px;
+    line-height: 1.29;
+  }
+`;
+
+const MoreInfo = styled.div`
+  width: 927px;
+  height: 227px;
+  margin-left: 225px;
+  ul {
+    li {
+      font-size: 23px;
+      line-height: 1.65;
+    }
+  }
+`;
+
+const Information = (props) => {
+  return (
+    <Description>
+      <DescriptTitle>
+        <h2>Re-build</h2>
+      </DescriptTitle>
+      <Descript>
+        <p>
+          One advanced diverted domestic sex repeated bringing you old. Possible
+          procured her trifling laughter thoughts property she met way.
+          Companions shy had solicitude favourable own. Which could saw guest
+          man now heard but. Lasted my coming uneasy marked so should. Gravity
+          letters it amongst herself dearest an windows by. Wooded ladies she
+          basket season age her uneasy saw. Discourse unwilling am no described
+          dejection incommode no listening of. Before nature his parish boy.
+        </p>
+      </Descript>
+      <MoreInfo>
+        <ul>
+          <li>One advanced diverted domestic sex repeated bringing you old</li>
+          <li>Possible procured her trifling laughter thoughts property </li>
+          <li>
+            Companions shy had solicitude favourable own. Which could saw guest
+            man now{" "}
+          </li>
+          <li>
+            Lasted my coming uneasy marked so should. Gravity letters it amongst
+            herself dearest{" "}
+          </li>
+        </ul>
+      </MoreInfo>
+    </Description>
+  );
+};
+
+export default Information;
--- src/components/Maintenance.jsx
+++ src/components/Maintenance.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import Carousel from "./Carousel";
 import styled from "styled-components";
 import SecondaryNav from "./SecondaryNav";
-
+import Information from "./Information";
 import Tranny from "../assets/active_new_trany.jpg";
 
 const Wrapper = styled.div``;
@@ -96,46 +96,6 @@ const SecondNav = styled.div`
   background: rgba(36, 39, 140, 0.91);
 `;
 
-const Description = styled.div`
-  width: 100%;
-  height: 752px;
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-`;
-const DescriptTitle = styled.div`
-  width: 218px;
-  height: 59px;
-  margin-left: 160px;
-  margin-bottom: 32px;
-  h2 {
-    margin: 0;
-    font-size: 51px;
-  }
-`;
-
-const Descript = styled.div`
-  width: 1038px;
-  height: 152px;
-  margin-left: 169px;
-  margin-bottom: 32px;
-  p {
-    font-size: 24px;
-    line-height: 1.29;
-  }
-`;
-
-const MoreInfo = styled.div`
-  width: 927px;
-  height: 227px;
-  margin-left: 225px;
-  ul {
-    li {
-      font-size: 23px;
-      line-height: 1.65;
-    }
-  }
-`;
 const Maintenance = (props) => {
   const [maiNav, setMaiNav] = useState(1);
   const [mainViewOptions, setMainViewOption] = useState(1);
@@ -166,39 +126,7 @@ const Maintenance = (props) => {
           <SecondaryNav mainOptions={mainViewOptions} />
         </SecondNav>
       </Nav>
-      <Description>
-        <DescriptTitle>
-          <h2>Re-build</h2>
-        </DescriptTitle>
-        <Descript>
-          <p>
-            One advanced diverted domestic sex repeated bringing you old.
-            Possible procured her trifling laughter thoughts property she met
-            way. Companions shy had solicitude favourable own. Which could saw
-            guest man now heard but. Lasted my coming uneasy marked so should.
-            Gravity letters it amongst herself dearest an windows by. Wooded
-            ladies she basket season age her uneasy saw. Discourse unwilling am
-            no described dejection incommode no listening of. Before nature his
-            parish boy.
-          </p>
-        </Descript>
-        <MoreInfo>
-          <ul>
-            <li>
-              One advanced diverted domestic sex repeated bringing you old
-            </li>
-            <li>Possible procured her trifling laughter thoughts property </li>
-            <li>
-              Companions shy had solicitude favourable own. Which could saw
-              guest man now{" "}
-            </li>
-            <li>
-              Lasted my coming uneasy marked so should. Gravity letters it
-              amongst herself dearest{" "}
-            </li>
-          </ul>
-        </MoreInfo>
-      </Description>
+      <Information />
     </Wrapper>
   );
 };
