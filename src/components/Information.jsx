fa46892e04560d41e5075a66bd994790c1db6dd0
RobnettSean22 <robnettsean22@gmail.com>
2021-01-09 23:13:53 +0900

    displaying data

--- src/components/Information.jsx
+++ src/components/Information.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 import styled from "styled-components";
 
 const Description = styled.div`
@@ -43,22 +44,26 @@ const MoreInfo = styled.div`
 `;
 
 const Information = (props) => {
-  const [info, setInfo] = useState([]);
+  const [info, setInfo] = useState([{}]);
+
+  useEffect(() => {
+    serviceInfo();
+  }, []);
+
+  const serviceInfo = async () => {
+    const res = await axios.get("/info");
+    const { data } = await res;
+    console.log(data);
+    setInfo(data);
+  };
+
   return (
     <Description>
       <DescriptTitle>
-        <h2>Re-build</h2>
+        <h2>{info[0].title}</h2>
       </DescriptTitle>
       <Descript>
-        <p>
-          One advanced diverted domestic sex repeated bringing you old. Possible
-          procured her trifling laughter thoughts property she met way.
-          Companions shy had solicitude favourable own. Which could saw guest
-          man now heard but. Lasted my coming uneasy marked so should. Gravity
-          letters it amongst herself dearest an windows by. Wooded ladies she
-          basket season age her uneasy saw. Discourse unwilling am no described
-          dejection incommode no listening of. Before nature his parish boy.
-        </p>
+        <p>{info[0].serviceDescription}</p>
       </Descript>
       <MoreInfo>
         <ul>
