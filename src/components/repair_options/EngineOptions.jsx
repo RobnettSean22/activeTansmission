5ad5fd59f4664027a4df55795f21edfe9e724be9
RobnettSean22 <robnettsean22@gmail.com>
2021-01-11 01:18:18 +0900

    redesign to secondary buttons... making changes

--- src/components/repair_options/EngineOptions.jsx
+++ src/components/repair_options/EngineOptions.jsx
@@ -3,22 +3,22 @@ import styled from "styled-components";
 
 const Wrapper = styled.div`
   width: 100%;
-  height: 100%;
+  height: 50%;
   display: flex;
+  background: yellow;
   justify-content: space-evenly;
   align-items: center;
 `;
 
 const Capsule = styled.div`
   width: 130px;
-  height: 130px;
+  height: 100%;
   display: flex;
   justify-content: flex-start;
   align-items: center;
   flex-direction: column;
-  margin-top: 42px;
+  background: pink;
   h5 {
-    margin-top: 6px;
   }
 `;
 
