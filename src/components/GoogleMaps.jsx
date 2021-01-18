f5ce16e00403f41713881b45e57fe141d9163f7a
RobnettSean22 <robnettsean22@gmail.com>
2021-01-18 18:05:02 +0900

    added marker spacing and b radius and shadow to map

--- src/components/GoogleMaps.jsx
+++ src/components/GoogleMaps.jsx
@@ -1,7 +1,6 @@
 import React, { useLayoutEffect } from "react";
 
 const GoogleMaps = (props) => {
-  const { REACT_APP_MAP_KEY } = process.env;
   useLayoutEffect(() => {
     if (window.google) {
       window.initMap = onScriptLoad();
@@ -9,7 +8,7 @@ const GoogleMaps = (props) => {
     let googleScript = document.createElement("script");
     googleScript.type = "type/javascript";
     googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${REACT_APP_MAP_KEY}`;
-  
+
     googleScript.defer = true;
     document.head.appendChild(googleScript);
     let pickUpTag = document.getElementsByTagName("script")[0];
@@ -18,20 +17,30 @@ const GoogleMaps = (props) => {
       onScriptLoad();
     });
   }, []);
+  const { REACT_APP_MAP_KEY } = process.env;
+
   const onScriptLoad = () => {
+    const myLatLng = { lat: 39.91084, lng: -105.07783 };
     const mapId = document.getElementById("map");
     const map = new window.google.maps.Map(mapId, {
       options: {
-        center: { lat: 39.91157, lng: -105.07813 },
-        zoom: 15,
+        center: myLatLng,
+        zoom: 17,
       },
+    });
+    new window.google.maps.Marker({
+      position: myLatLng,
+      map,
+      title: "Active Transimission and Gear",
     });
   };
   return (
     <div
       id='map'
       style={{
-        background: "blue",
+        boxShadow: "0 3px 6px 0 rgba(0, 0, 0, 0.9)",
+        marginLeft: "142px",
+        borderRadius: "15px",
         height: "516px",
         width: "624px",
       }}
